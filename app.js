a419d09331d41a30ccd5d0582b8842d4d962a41f
jeanmarcj <jmjbup@gmail.com>
2021-08-24 17:32:11 +0200

    Feat. Add cors middleware for crosssite authorization.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 // Database
 const db = require('./models/index');
