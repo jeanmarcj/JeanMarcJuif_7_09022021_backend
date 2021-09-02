669e8e035f889377a824d4f02e8f7d61fe29bd6e
jeanmarcj <jmjbup@gmail.com>
2021-09-02 16:08:42 +0200

    Feat. Add retrieve last uploaded file

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,37 +1,61 @@
 const uploadFile = require("../middleware/multer-config.js");
-const db = require('../models');
-const Post = db.posts;
 const fs = require('fs');
+const path = require('path');
 
 const upload = async (req, res) => {
   try {
 
-        console.log(req.file);
+        // console.log(req.file);
 
         await uploadFile(req, res);
 
         if (req.file == undefined) {
-        return res.status(400).send({ message: "Please select a file!" });
-        }
+          return res.status(400).send({ message: "Sélectionner un fichier !" });
+          }
+        
+        // Get the lastet file named
+        const getMostRecentFile = (dir) => {
+          const files = orderReccentFiles(dir);
+          return files.length ? files[0] : undefined;
+        };
+      
+        const orderReccentFiles = (dir) => {
+            return fs.readdirSync(dir)
+                .filter(file => fs.lstatSync(path.join(dir, file)).isFile())
+                .map(file => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
+                .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+        };
+      
+        const dirPath = 'images';
 
-    res.status(200).send({
-      message: "Uploaded the file successfully: " + req.file.originalname,
-    });
-  } catch (err) {
-    res.status(500).send({
-      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
-    });
-  }
+        const filename = getMostRecentFile(dirPath);
+        console.log('My file name ', filename);
+        
+        res.status(200).json({
+          message: "Fichier téléchargé avec succés !",
+          fileName: filename,
+      });
+
+        // res.status(200).send({
+        //   message: "Fichier téléchargé avec succès: " + req.file.originalname,
+        // });
+
+      } catch (err) {
+          res.status(500).send({
+            message: `Impossible de télécharger le fichier : ${req.file.originalname}. ${err}`,
+          });
+      }
 };
 
 const getListFiles = (req, res) => {
+
     const directoryPath = __basedir + "/images/";
     const baseUrl = "http://localhost:3000/images/";
 
     fs.readdir(directoryPath, function (err, files) {
       if (err) {
         res.status(500).send({
-          message: "Unable to scan files!",
+          message: "Impossible de lire les fichiers !"
         });
       }
   
@@ -48,6 +72,7 @@ const getListFiles = (req, res) => {
     });
   };
 
+
   const download = (req, res) => {
     const fileName = req.params.name;
     const directoryPath = __basedir + "/images/";
@@ -55,7 +80,7 @@ const getListFiles = (req, res) => {
     res.download(directoryPath + fileName, fileName, (err) => {
       if (err) {
         res.status(500).send({
-          message: "Could not download the file. " + err,
+          message: "Une erreur est intervenue pendant le téléchargement du fichier. Erreur : " + err
         });
       }
     });
